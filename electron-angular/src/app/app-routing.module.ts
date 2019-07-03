13a570c8c05e2da5a098b3148e77d40aa1d793e6
sy <yisui@skspruce.net>
2019-07-03 18:39:27 +0800

    初始化

---
+++ electron-angular/src/app/app-routing.module.ts
@@ -0,0 +1,26 @@
+import { HomeComponent } from './components/home/home.component';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { AboutComponent } from './components/about/about.component';
+
+const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full'
+    },
+    {
+        path: 'home',
+        component: HomeComponent
+    },
+    {
+        path: 'about',
+        component: AboutComponent
+    }
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes, {useHash: true})],
+    exports: [RouterModule]
+})
+export class AppRoutingModule { }
